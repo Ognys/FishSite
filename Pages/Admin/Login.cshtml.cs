36b0de0262b19049b534791241fc6a84b09cc5c4
Ogny <ogny9@mail.ru>
2026-01-10 17:42:52 +0300

    Redesigned admin panel UI (auth, create, update pages)

--- Pages/Admin/Login.cshtml.cs
+++ Pages/Admin/Login.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace FishProj.Pages;
@@ -19,32 +20,34 @@ public class LoginModel : PageModel
     }
 
     [BindProperty]
+    [Display(Name = "Логин")]
     public string Name { get; set; }
     [BindProperty]
+    [Display(Name = "Пароль")]
     public string Password { get; set; }
 
-    public void OnGet() {}
+    public void OnGet() { }
 
     public async Task<IActionResult> OnPostAsync()
     {
         var AdminUser = _conf["Admin:UserName"];
         var AdminPass = _conf["Admin:Password"];
 
-        if(string.IsNullOrWhiteSpace(AdminUser) || string.IsNullOrWhiteSpace(AdminPass))
+        if (string.IsNullOrWhiteSpace(AdminUser) || string.IsNullOrWhiteSpace(AdminPass))
         {
-            ModelState.AddModelError("","Конфигурация админ-панели не настроена");
+            ModelState.AddModelError("", "Конфигурация админ-панели не настроена");
             return Page();
         }
 
-        if(Name != AdminUser || Password != AdminPass)
+        if (Name != AdminUser || Password != AdminPass)
         {
-            ModelState.AddModelError("","Неверные учетные данные");
+            ModelState.AddModelError("", "Неверные учетные данные");
             return Page();
         }
 
         var Claims = new List<Claim>
         {
-          new Claim(ClaimTypes.Name, Name),  
+          new Claim(ClaimTypes.Name, Name),
         };
 
         var identity = new ClaimsIdentity(Claims, CookieAuthenticationDefaults.AuthenticationScheme);
@@ -52,6 +55,6 @@ public class LoginModel : PageModel
                         CookieAuthenticationDefaults.AuthenticationScheme,
             new ClaimsPrincipal(identity));
 
-            return RedirectToPage("/Admin/Add");
+        return RedirectToPage("/Admin/Add");
     }
 }
